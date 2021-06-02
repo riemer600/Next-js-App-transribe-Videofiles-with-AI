6973bbc6d8ca82e9706f78a9136fb908bc3d9c5e
Ben Riemer <benjamin@riemer-berlin.de>
2021-06-02 08:23:11 +0200

    Created a simple Video Processing App using AI to transcribe Text from a Videofile

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import { AuthProvider } from '../hooks'
+// 2. Extend the theme to include custom colors, fonts, etc
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const theme = extendTheme({ colors })
+
+function MyApp ({ Component, pageProps }) {
+  return (
+    <AuthProvider>
+      <ChakraProvider resetCSS theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </AuthProvider>
+  )
 }
 
 export default MyApp
